87948c5cc0e832d99429f8227fc5fd314b891d9e
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-19 19:05:46 +0700

    feat(todo_form.dart): for edit and add todo with form

---
+++ lib/widgets/todo_form.dart
@@ -0,0 +1,87 @@
+import 'package:flodo/models/todo.dart';
+import 'package:flodo/pages/controller.dart';
+import 'package:flutter/material.dart';
+
+class TodoForm extends StatefulWidget {
+  final ControllerMode mode;
+  final Function(Todo) onSubmit;
+  final Todo? initialTodo;
+
+  const TodoForm({
+    Key? key, 
+    required this.mode,
+    required this.onSubmit,
+    this.initialTodo
+  }) : super(key: key);
+
+  @override
+  TodoFormState createState() {
+    return TodoFormState();
+  }
+}
+
+class TodoFormState extends State<TodoForm> {
+  final _formKey = GlobalKey<FormState>();
+  bool _isFinish = false;
+  TextEditingController name = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _isFinish = widget.initialTodo?.isFinish ?? false;
+    name = TextEditingController(text: widget.initialTodo?.name);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String buttonText = widget.mode == ControllerMode.add ? "Add Todo" : "Update Todo";
+
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            controller: name,
+            decoration: const InputDecoration(
+              labelText: "Todo",
+              contentPadding: EdgeInsets.symmetric(vertical: 5)
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Please fill this form";
+              }
+
+              return null;
+            },
+          ),
+          const SizedBox(height: 10),
+          SwitchListTile(
+            value: _isFinish,
+            title: const Text("Finish ?"),
+            onChanged: (value) {
+              setState(() {
+                _isFinish = value;
+              });
+            }
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              if (!_formKey.currentState!.validate()) {
+                return;
+              }
+
+              Todo todo = Todo(
+                name: name.text,
+                isFinish: _isFinish
+              );
+              widget.onSubmit(todo);
+            },
+            child: Text(buttonText),
+          )
+        ],
+      ),
+    );
+  }
+}
