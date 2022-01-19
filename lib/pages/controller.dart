7588d12c05f4d29543f01e3e0af498f8ded3de7b
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-01-19 19:06:10 +0700

    feat(controller.dart): feature to add todo

--- lib/pages/controller.dart
+++ lib/pages/controller.dart
@@ -1,11 +1,15 @@
 import 'package:flodo/layouts/base_layout.dart';
+import 'package:flodo/models/todo.dart';
+import 'package:flodo/providers/todo_provider.dart';
+import 'package:flodo/widgets/todo_form.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
 enum ControllerMode { add, update }
 
 /// Controller page for update or add todo
 class Controller extends StatelessWidget {
-  final ControllerMode? mode;
+  final ControllerMode mode;
 
   const Controller({Key? key, required this.mode}): super(key: key);
 
@@ -15,7 +19,15 @@ class Controller extends StatelessWidget {
 
     return BaseLayout(
       title: title, 
-      body: const Text("Add todo")
+      body: TodoForm(
+        mode: mode,
+        onSubmit: (todo) {
+          if (mode == ControllerMode.add) {
+            context.read<TodoProvider>().add(todo);
+          }
+          Navigator.pop(context);
+        },
+      )
     );
   }
 }
